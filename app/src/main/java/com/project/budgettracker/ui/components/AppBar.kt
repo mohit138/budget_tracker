986cd598f35d27df04bb5a3bda7a3db3586dd2a4
mohit138 <mohit22gidwani@gmail.com>
2025-11-02 00:46:42 +0530

    Using all default colors

--- app/src/main/java/com/project/budgettracker/ui/components/AppBar.kt
+++ app/src/main/java/com/project/budgettracker/ui/components/AppBar.kt
@@ -6,10 +6,8 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,9 +23,6 @@ fun AppBar(onNavigationIconClick: () -> Unit, onTitleClick: () -> Unit) {
                 modifier = Modifier.clickable(onClick = onTitleClick)
             )
         },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = MaterialTheme.colorScheme.onPrimary
-        ),
         navigationIcon = {
             IconButton(onClick = onNavigationIconClick) {
                 Icon(
